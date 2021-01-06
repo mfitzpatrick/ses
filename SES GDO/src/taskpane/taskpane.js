4997ce2e6197029d3e7051e96c794956e1fe3d17
Matt Fitzpatrick <1657422+mfitzpatrick@users.noreply.github.com>
2021-01-07 04:35:15 +1000

    Run task pane contents automatically

--- SES GDO/src/taskpane/taskpane.js
+++ SES GDO/src/taskpane/taskpane.js
@@ -14,7 +14,8 @@ Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    // document.getElementById("run").onclick = run;
+    run();
   }
 });
 
